0faa2a2902b7f61f9d0f96fd5a286313da97c986
juthomas <juthomas@student.42.fr>
2020-11-04 23:32:09 +0100

    [ADD] bugged test

---
+++ Original/sketch.js
@@ -0,0 +1,26 @@
+// let originalP1;
+// var spacing = 180;
+
+function preload() {
+    // originalP1 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep1v2.obj", true);
+
+}
+
+function setup() {
+    // createCanvas(1240, 800, WEBGL);
+}
+
+function draw() {
+  // background(200);
+  // scale(1);
+  // rotateY(frameCount * 0.11);
+  //  rotateX(frameCount * 0.01);
+  //  rotateZ(3.14/2);
+  // normalMaterial();
+// stroke(100, 180, 100);
+  // translate(0, -0.1, 0);
+  // model(originalP1);
+
+
+
+}
---
+++ Spine/sketch.js
@@ -0,0 +1,41 @@
+let spineP1;
+let spineP2;
+let spineP3;
+let spineP4;
+let spineP5;
+var spacing = 180;
+
+function preload() {
+    spineP1 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep1v2.obj", true);
+    spineP2 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep2v2.obj", true);
+    spineP3 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep3.obj", true);
+    spineP4 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep4.obj", true);
+    spineP5 = loadModel("https://raw.githubusercontent.com/juthomas/P5-Spine-3d/main/TripodeSpinep5.obj", true);
+}
+
+function setup() {
+    createCanvas(1240, 800, WEBGL);
+}
+
+function draw() {
+  background(200);
+  scale(1);
+  rotateY(frameCount * 0.01);
+   rotateX(frameCount * 0.01);
+   rotateZ(3.14/2);
+  normalMaterial();
+// stroke(100, 180, 100);
+  translate(0, -spacing * 1.98, 0);
+  model(spineP1);
+  translate(0, spacing, 0);
+  model(spineP2);
+  translate(0, spacing, 0);
+  model(spineP3);
+  translate(0, spacing, 0);
+  model(spineP4);
+
+  translate(0, spacing, 0);
+  model(spineP5);
+
+
+}
